8cf99ad1e89531e2ed99fabae7a3e3be599ef228
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-23 18:56:50 +0200

    forgot sound

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ class $modify(PlayLayer) {
     void showNewBest(bool newReward, int orbs, int diamonds, bool demonKey, bool noRetry, bool noTitle) {
         if (Mod::get()->getSettingValue<bool>("replace-new-best")) {
             auto icon = CCSprite::create(geode::utils::string::pathToString(Mod::get()->getResourcesDir() / "just.png").c_str());
-            notifapi::fnotif("New Best! " + std::to_string((int)(m_level->m_normalPercent)) + "%", "warning", 3.0f, {0,0,0}, 1.0f, notifapi::Position::TopRight, notifapi::Animation::Slide, "", 1.0f, icon);
+            notifapi::fnotif("New Best! " + std::to_string((int)(m_level->m_normalPercent)) + "%", "warning", 3.0f, {0,0,0}, 1.0f, notifapi::Position::TopRight, notifapi::Animation::Slide, "achievement_01.ogg", 1.0f, icon);
         } else {
             PlayLayer::showNewBest(newReward, orbs, diamonds, demonKey, noRetry, noTitle);
         }
