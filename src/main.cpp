60ec2473e0196019405694501a25af9c72a63ef7
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-05-23 18:05:05 +0200

    first

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include "../includes/includes/notif.hpp"
+
+using namespace geode::prelude;
+
+class $modify(PlayLayer) {
+    void showNewBest(bool newReward, int orbs, int diamonds, bool demonKey, bool noRetry, bool noTitle) {
+        if (Mod::get()->getSettingValue<bool>("replace-new-best")) {
+            auto icon = CCSprite::create(geode::utils::string::pathToString(Mod::get()->getResourcesDir() / "just.png").c_str());
+            notifapi::fnotif("New Best! " + std::to_string((int)(m_level->m_normalPercent)) + "%", "warning", 3.0f, {0,0,0}, 1.0f, notifapi::Position::TopRight, notifapi::Animation::Slide, "", 1.0f, icon);
+        } else {
+            PlayLayer::showNewBest(newReward, orbs, diamonds, demonKey, noRetry, noTitle);
+        }
+    }
+};
